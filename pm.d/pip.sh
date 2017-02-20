3adcf69f7b66acffa4b0897649457474f238fec9
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2017-02-20 21:19:37 +0000

    fix(pm-pip): add sudo

--- pm.d/pip.sh
+++ pm.d/pip.sh
@@ -10,7 +10,7 @@ adm_pm__pip() {
     local packages=( ${args[@]:1} )
 
     case $command in
-        install) pip install "${PIP_FLAGS[@]}" "${packages[@]}" ;;
+        install) sudo pip install "${PIP_FLAGS[@]}" "${packages[@]}" ;;
         *)       err "Invalid command: $command"; return 1 ;;
     esac
 }
