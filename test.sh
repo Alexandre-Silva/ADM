1316d23c3e60d419d6984cd27cb93d459ac599a0
Alexandre-Silva <alexandre.simoesilva@gmail.com>
2017-12-14 13:39:20 +0000

    feat(test): tests both bash and zsh

--- test.sh
+++ test.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-NO_COLOR=t shpec/bin/shpec ./test.shpec.sh
+echo ">> Testing in bash <<"
+NO_COLOR=t bash shpec/bin/shpec ./test.shpec.sh
+echo
+
+echo ">> Testing in zsh <<"
+NO_COLOR=t zsh -c 'disable -r end; . $(dirname $0:A)/shpec/bin/shpec ./test.shpec.sh'
+echo
