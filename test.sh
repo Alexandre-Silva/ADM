f58627fe90be70e8fafcb13906b445605ea6417e
Alexandre-Silva <alexandre.simoesilva@gmail.com>
2017-12-14 17:32:17 +0000

    feat(test): now has target all

--- test.sh
+++ test.sh
@@ -1,11 +1,27 @@
-#!/bin/bash
+#!/usr/bin/bash
+
+RUN_BASH=0
+RUN_ZSH=0
+
+for arg in "$@"; do
+    case $arg in
+        bash|all) RUN_BASH=1 ;;&
+        zsh|all) RUN_ZSH=1 ;;&
+        bash|zsh|all) ;;
+        *) echo "ERROR: Unrecognized option"; exit 1 ;;
+    esac
+done
 
 export SHPEC_ROOT="$PWD"
 
-echo ">> Testing in bash <<"
-NO_COLOR=t bash shpec/bin/shpec ./test.shpec.sh
-echo
+if ((RUN_BASH)); then
+    echo ">> Testing in bash <<"
+    NO_COLOR=t bash shpec/bin/shpec ./test.shpec.sh
+    echo
+fi
 
-echo ">> Testing in zsh <<"
-NO_COLOR=t zsh -c 'disable -r end; . $(dirname $0:A)/shpec/bin/shpec ./test.shpec.sh'
-echo
+if ((RUN_ZSH)); then
+    echo ">> Testing in zsh <<"
+    NO_COLOR=t zsh -c 'disable -r end; . $(dirname $0:A)/shpec/bin/shpec ./test.shpec.sh'
+    echo
+fi
