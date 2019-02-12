5200a1efdc6f83d03e3b4feca312edb2a6ee7cfc
Alexandre-Silva <alexandre.s.silva@posteo.net>
2019-02-12 09:24:10 +0000

    refac(pm): change usage of pacaur to pikaur due to loss of maintainer

---
+++ pm.d/pikaur.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+PIKAUR_FLAGS=( --silent --noedit --noconfirm --noprogressbar --needed )
+TO_BE_UNSET+=( "PIKAUR_FLAGS" )
+
+adm_pm__pikaur() {
+    local args=( "$@" )
+    local command=${args[0]}
+    local packages=( ${args[@]:1} )
+
+    case $command in
+        install) pikaur "${PIKAUR_FLAGS[@]}" --sync "${packages[@]}" ;;
+        remove)  sudo pacman -Rns "${packages[@]}" ;;
+        *)       err "Invalid command: $command"; return 1 ;;
+    esac
+}
+
+if hash pikaur &>/dev/null; then
+    adm_pm_register "aur" "adm_pm__pikaur"
+fi
